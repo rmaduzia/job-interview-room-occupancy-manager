ec6f7e0ebfb1d55103fbd43da579850bd50a84c8
rmaduzia <rafi11m@hotmail.com>
2024-06-24 17:42:03 +0200

    room-occupancy-manager - add Swagger

--- src/main/java/com/example/room/occupancy/manager/ocuppancy/OccupancyController.java
+++ src/main/java/com/example/room/occupancy/manager/ocuppancy/OccupancyController.java
@@ -1,5 +1,7 @@
 package com.example.room.occupancy.manager.ocuppancy;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,12 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Occupancy Calculator", description = "Room Manager Occupancy Calculator")
 @RequestMapping("/api/occupancy")
 @RequiredArgsConstructor
 public class OccupancyController {
 
     private final OccupancyService occupancyService;
 
+    @Operation(summary = "Calculate Occupancy for Guests")
     @PostMapping
     public OccupancyResult optimizeOccupancy(@RequestBody CalculateOccupancyRequest calculateOccupancyRequest) {
         return occupancyService.optimizeOccupancy(calculateOccupancyRequest);
